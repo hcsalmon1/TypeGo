a06993561b3c0f949c7f1bd93b337cfc84c6a9e6
Coding with Tom <146443103+hcsalmon1@users.noreply.github.com>
2026-01-06 21:04:21 +0000

    Update function_utils.go

--- src/converting/function_utils.go
+++ src/converting/function_utils.go
@@ -17,7 +17,8 @@ func AddParameter(parameters []Variable, tempParameter *Variable) {
 	
 		parameterCopy.TypeList = append(parameterCopy.TypeList, tempParameter.TypeList[i])
 		
-			}
+		
+	}
 	
 	parameterCopy.NameToken = CopyTokenList(tempParameter.NameToken); 
 	
@@ -118,7 +119,8 @@ func FindParameters(formatData *FormatData, parameters []Variable) {
 		if iterationResult == LoopAction.Return {
 			return; 
 		}
-			}
+		
+	}
 	
 	
 }
